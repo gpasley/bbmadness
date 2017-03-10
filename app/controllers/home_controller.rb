5ffff0bb644f89b2409c011a7d127c1af9e9e7e6
George Pasley <gpasley@imazuma.com>
2017-03-10 17:03:46 -0500

    First commit for Brackets

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  
+  def index
+    # if user_signed_in?
+    #   @picks = Sheet.count(:group => :round6, :order => :round6, :conditions => {:is_master => false})
+    #   @sheet = Sheet.where("is_master = ?", true).first
+    #   @leaders = Sheet.where("is_master = ?", false).order("total_points desc").limit(10)
+    #   @locked = Sheet.where("is_locked = ?", true).first
+    # else
+    #   @page = 'front'
+    #   redirect_to new_user_session_path
+    # end
+  end
+  
+end
