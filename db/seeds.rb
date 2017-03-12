065ddf6c47d7dd93c05b48f4a141a8c566622b17
George Pasley <gpasley@imazuma.com>
2017-03-12 16:01:49 -0400

    Seeds update
    - Added seeds for easy import of teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Team.delete_all
+
 teams = Team.create(
 [
   {name: 'Duke',  rank: 1,  match_up: 1, group: 'A'},
