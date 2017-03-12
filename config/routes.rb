2c5df30c346ac904d05251c81f76a5e62ac1fb2d
George Pasley <gpasley@imazuma.com>
2017-03-12 13:00:36 -0400

    User permitted attributes
    - Added first_name and last_name to registration permitted attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'sheets/my_brackets' => 'sheets#my_brackets', :as => :my_brackets
